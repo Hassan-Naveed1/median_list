49426455704425af78120dd2bea603c4b2f96d3d
Hassan-Naveed1 <hassann4242@gmail.com>
2025-11-16 00:22:23 +0000

    Update median.py

--- median.py
+++ median.py
@@ -1,16 +1,26 @@
 def median(median_list):
 
-    new_list = []
-    while median_list:
-        smallest = median_list[0]
-        for i in median_list:
-            if i < smallest:
-                smallest = i 
-        print(median_list)
-        new_list.append(smallest)
-        median_list.remove(smallest)
+  if len(median_list) == 1:
+    return median_list[0]
+  new_list = []
+  while median_list:
+    smallest = median_list[0]
+    for i in median_list:
+      if i < smallest:
+        smallest = i
+    new_list.append(smallest)
+    median_list.remove(smallest)
 
-    return new_list
+  if len(new_list) % 2 != 0:
+    mid_item = len(new_list) // 2
+    return new_list[mid_item]
 
-print(median([1,2,3,55,6,2,1]))
+  elif len(new_list) % 2 == 0:
+    mid_item1 = len(new_list) // 2
+    mid_item2 = mid_item1 - 1
+    avrg_mid_items = (new_list[mid_item1] + new_list[mid_item2]) / 2
 
+    return avrg_mid_items
+
+
+print(median([4,5,5,4]))
